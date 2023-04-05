b18d473b053f7eb01867d78ab1349295dc46629a
StefanoGalandrini <stefe68@gmail.com>
2023-04-05 14:31:03 +0200

    Add distance and age request, calculate wholePrice

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,19 @@
 /*
-
+	- chiedere il numero di Km da percorrere e inserirla nella variabile distance;
+	- chiedere l'età del passeggero e inserirla nella variabile age;
+	- SE distance e age sono numeri e sono maggiori di 0:
+		- moltiplicare distance per 0.21 e inserire il risultato nella variabile wholePrice;
+		- stampare: "Prezzo pieno: euro" + wholePrice
+		- SE age < 18:
+			- moltiplicare wholePrice per 0.2 e inserire il risultato nella variabile discountedPrice;
+			- stampare: "Il viaggiatore è minorenne: il prezzo scontato del 20% è di euro" + discountedPrice;
+			ALTRIMENTI SE age > 65:
+				- moltiplicare wholePrice per 0.4 e inserire il risultato nella variabile discountedPrice;
+				- stampare: "Il viaggiatore è over-65: il prezzo scontato del 40% è di euro" + discountedPrice;
+			FINE SE
+		FINE SE
+		- stampare: "Non si ha diritto ad alcuno sconto"
+	FINE SE
+	- stampare: "Uno o entrambi i valori inseriti non sono validi"
 */
 
